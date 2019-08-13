5f57dcaee6ef1d10d0b4c1706d2a83da8da53184
prasanth <prasanth91097@gmail.com>
2019-08-13 13:51:04 +0530

    i have company 8

---
+++ prasanth/company8/company8/EmployeeSalary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace company8
+{
+    class EmployeeSalary
+    {
+        int EmpId;
+        String employeename;
+        int salary;
+        int basic;
+        int travelallowance;
+        int HRallowance;
+        public EmployeeSalary(int basic0, int tr, int hr)
+        {
+            basic= basic0;
+            travelallowance = tr;
+            HRallowance = hr;
+
+        }
+        
+
+        public int getEmpId1()
+        {
+            return EmpId;
+        }
+        public void setEmpId(int id)
+        {
+            EmpId = id;
+        }
+        
+             public String getNAME()
+        {
+            return employeename;
+        }
+        public void setNAME(String employee)
+        {
+            employeename = employee;
+        }
+    }
+       
+        
+        }
+   
---
+++ prasanth/company8/company8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace company8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeSalary E = new EmployeeSalary(10000, 5000, 600);
+            E.setEmpId(123545);
+            Console.WriteLine($"employee id={E.getEmpId1()}");
+            E.setNAME("prasanth");
+            Console.WriteLine($"employee name={ E.getEmpId1()}");
+            String s = Console.ReadLine();
+            int status =(int)marrital.s;
+            enum marrital
+        {
+            married, unmarried,complicated,widow,divorse
+
+
+        };
+            
+
+        }
+    }
+}
